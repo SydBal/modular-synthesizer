f99e92cf29e1ab79d5397e795b35c26199478db5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-10 13:09:44 +0000

    Deploy SydBal/modular-synthesizer to github.com/SydBal/modular-synthesizer.git:gh-pages

---
+++ precache-manifest.37e4bab7a748daf26d61c6cd72329fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d1200f485328cd0b4a04ed73a29ad5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47178104f5c2f9c28d56",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fe9beca0193cbe6b3fb1",
+    "url": "./static/js/2.19b4614e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.19b4614e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47178104f5c2f9c28d56",
+    "url": "./static/js/main.08e8e8c0.chunk.js"
+  },
+  {
+    "revision": "80ff7c7194cb3a25d449",
+    "url": "./static/js/runtime-main.7807e49c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c1f59ed9832676486e9dcaecfcf38c8.js"
+  "./precache-manifest.37e4bab7a748daf26d61c6cd72329fec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
